a4308f9accd1c0ccdce02e525644ee7aeb27d35b
Shehu Yusuf <yusufelpercy@gmail.com>
2026-08-11 21:43:57 +0100

    feat: complete middleware - two-way sync Django and Firestore

--- core/celery.py
+++ core/celery.py
@@ -1,3 +1,4 @@
+# core/celery.py
 import os
 from celery import Celery
 
@@ -6,3 +7,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 app = Celery("sync_middleware")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+# Explicitly import tasks
+import tasks.celery_tasks
--- core/settings.py
+++ core/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "queue_app",
     "engine",
     "api",
+    "tasks",
 ]
 
 MIDDLEWARE = [
